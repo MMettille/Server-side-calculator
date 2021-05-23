b8978fc12c7487e315b07a88092aad73cd0342bc
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-23 10:10:18 -0500

    buttons buttons buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,45 @@ function readyNow(){
     // checking to see that jQuery has been loaded and it is safe to manipulate the DOM
     console.log('in readyNow function')
     getHistory();
+    // buttons
+    $(  '#plusBtn' ).on('click', addition);
+    $( '#subtractBtn' ).on('click', subtract);
+    $( '#multiplyBtn' ).on('click', multiply);
+    $( '#divideBtn' ).on('click', divide);
+    $( '#equalsBtn' ).on('click', mathTime);
+    $( '#clearBtn' ).on('click', clear)
+}
+
+let operator;
+// button functionality
+function addition(){
+    // checking to see that the function is being called
+    console.log('in addition function');
+}
+
+function subtract(){
+    // checking to see that the function is being called
+    console.log('in subtract function');
+}
+
+function multiply(){
+    // checking to see that the function is being called
+    console.log('in multiply function');
+}
+
+function divide(){
+    // checking to see that the function is being called
+    console.log('in divide function');
+}
+
+function mathTime(){
+    // checking to see that the function is being called
+    console.log('in mathTime function');
+}
+
+function clear(){
+    // checking to see that the function is being called
+    console.log('in clear function');
 }
 
 function getHistory(){
